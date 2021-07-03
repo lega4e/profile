a8e86bf6552ff36ac7ccd018d132bf6d97bd66e4
nvxden <clever411@yandex.ru>
2021-07-03 16:48:15 +0300

    Added zshrc and i3

--- install.sh
+++ install.sh
@@ -2,12 +2,17 @@
 
 
 
-cp ./init.vim ~/.config/nvim/
-cp ./bashrc ~/.bashrc
-cp ./bashrc_custom ~/.bashrc_custom
-cp ./bash_aliases ~/.bash_aliases
-cp ./profile ~/.profile
-sudo cp ru /usr/share/X11/xkb/symbols
+cp ./init.vim        ~/.config/nvim/
+cp ./bashrc          ~/.bashrc
+cp ./bashrc_custom   ~/.bashrc_custom
+cp ./bash_aliases    ~/.bash_aliases
+cp ./profile         ~/.profile
+cp ./zshrc           ~/.zshrc
+cp ./zshrc_custom    ~/.zshrc_custom
+cp ./i3config        ~/.config/i3/config
+cp ./i3status_config ~/.config/i3status/config
+
+sudo cp ru                 /usr/share/X11/xkb/symbols
 sudo cp russian-dictor.vim /usr/share/vim/vim81/keymap
 
 
